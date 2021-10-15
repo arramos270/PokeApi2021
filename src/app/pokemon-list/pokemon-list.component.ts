8080dc0335e9488965a4537090ee11a175ba9471
arramos270 <ramirez.raale20@triana.salesianos.edu>
2021-10-15 12:37:35 +0200

    15-10

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Pokemon, PokemonListResponse } from '../interfaces/pokemon-list.interface';
+import { PokemonService } from '../services/pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css']
+})
+export class PokemonListComponent implements OnInit {
+  pokemonList: Pokemon[] | undefined;
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit(): void {
+    this.getPokemons(100);
+  }
+
+  getPokemons(pokemonLimit: number) {
+    this.pokemonService.getPokemonList(pokemonLimit).subscribe( resultado => {
+      this.pokemonList = resultado.results;
+      console.log(resultado);
+    });
+  }
+
+}
